1d3760d132e1f0a1eba9a91aaea99aa8efee9d0c
Israel Vasquez <israelvasquez3258@gmail.com>
2017-12-12 14:59:28 -0500

    Merge origin/master
    Conflicts:
    	src/orderprocess/OrderProcess.java

--- src/orderprocess/OrderProcess.java
+++ src/orderprocess/OrderProcess.java
@@ -22,35 +22,67 @@ public class OrderProcess {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(ReadWrite());
+        System.out.println(ReadWrite(tax(),shipping()));
         
         
     }
-    private static String ReadWrite()
+    private static String ReadWrite(double tax, double shipping)
     {
+        String read = "Orders.txt";
+        String write = "OrdersProcessed.txt";
         StringBuilder fileContent = new StringBuilder();/**used to store the lines read from the file.**/
         
-        try(BufferedReader myReader = new BufferedReader(new FileReader(new File("/Users/ivasquez/NetBeansProjects/Filereader/OrderProcess/src/orderprocess/Orders.txt"))))/** locate the text file for reading.**/
-        {                     
-            System.out.println("Start processing orders");/**begin manipulating text**/
-                BufferedWriter myWriter = new BufferedWriter(new FileWriter(new File("/Users/ivasquez/NetBeansProjects/Filereader/OrderProcess/src/orderprocess/OrdersProcessed.txt")));/**write to specified file.**/
-                    String line = myReader.readLine();/**the lines are read and stored into a string**/
-                        
-            while(line != null)/**until we finish reading the file, do the following.**/ 
+        try(BufferedReader myReader = new BufferedReader(new FileReader(new File("/Users/ivasquez/NetBeansProjects/Filereader/OrderProcess/src/orderprocess/"+ read))))/** locate the text file for reading.**/
+        {                
+            try(BufferedWriter myWriter = new BufferedWriter(new FileWriter(new File("/Users/ivasquez/NetBeansProjects/Filereader/OrderProcess/src/orderprocess/" + write))))/**write to specified file.**/
             {
-                fileContent.append(line);/**puts the line from the file into the StringBuilder**/
-                fileContent.append(System.lineSeparator());/**creates a new line in the StringBuilder for the next line to occupy**/
-                line = myReader.readLine();/**the file continues to be read.**/
+
+                myReader.readLine();/**reads the first line of the list**/
+                System.out.println("Start processing orders");/**tells the user that we have begun the process.**/
+                    String line = myReader.readLine();/**one line is read and stored into this string**/
+                
+                while(line != null)/**until we finish reading the file, do the following.**/ 
+                {
+                    
+                    
+                    String[] products = line.split("\\|");
+                    
+                    products[0] = "Order ID: "+" ";
+                    products[1] = "Order ID: " +" ";
+                    products[2] = "Order ID: " +" ";
+                    products[3] = "Order ID: " +" ";
+                    products[4] = "Order ID: " +" ";
+                    
+                 
+                    //fileContent.append();/**puts the line from the file into the StringBuilder**/
+                    fileContent.append(System.lineSeparator());/**creates a new line in the StringBuilder for the next string to occupy**/
+                    line = myReader.readLine();/**the file continues to be read.**/
+                }
+            }
+            catch(IOException e)/**displays file not found for writing error **/
+            {
+                System.out.println(e);
+                System.out.println("could not write to file "+ write);
             }
             
         }
         
-        catch(IOException e)
+        catch(IOException e)/**displays file not read error **/
         {
             System.out.println(e);
+            System.out.println("could not read file " + read);
         }
         return fileContent.toString();
     }
-   
+    
+    private static double tax(){
+       final double tax = .02;
+       return tax;
+   }
+    
+    private static double shipping(){
+       final double shipping = .05;
+       return shipping;
+   }
 }
 
